ee8ef0e18703af93689aaee1a1a252082ec1bc17
Sivasubramanyam A <ssdragonar5@gmail.com>
2019-08-29 18:39:48 +0530

    Update tests/manual.js

--- tests/manual.js
+++ tests/manual.js
@@ -2,7 +2,7 @@ const { htmlLint } = require('../index');
 
 (async() => {
   try {
-    await htmlLint('tests/sample-a.html');
+    await htmlLint('tests/sample-*.html');
   } catch (error) {
     console.log(error)
   }
