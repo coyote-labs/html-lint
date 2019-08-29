ed3846b30f4e4b8734e9097b9971595c16d33ac2
Sivasubramanyam A <ssdragonar5@gmail.com>
2019-08-29 18:48:14 +0530

    Add `spec-char-escape` rule (#20)
    * Add `spec-char-escape` rule
    
    * Update tests/manual.js

--- tests/rules/spec-char-escape/spec-char-escape.test.js
+++ tests/rules/spec-char-escape/spec-char-escape.test.js
@@ -5,10 +5,9 @@ global.console = {
 };
 
 test.each([
-  'tests/rules/inline-style-disabled/no-error.html',
-  'tests/rules/inline-style-disabled/error-inline-style-disabled.html',
+  'tests/rules/spec-char-escape/error.html',
 ])(
-  'inline-style-disabled %s',
+  'spec-char-escape %s',
   async(fixture) => {
     try {
       await htmlLint(fixture);
