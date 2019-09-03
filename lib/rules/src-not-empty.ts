ef343455d4ec786092b10b07d2732720474084c3
kandhavivekraj <kandhavivekraj@gmail.com>
2019-09-03 13:33:49 +0530

    rule: src attr for other tags

--- lib/rules/src-not-empty.ts
+++ lib/rules/src-not-empty.ts
@@ -1,5 +1,16 @@
 import { BaseRule, Options } from './base-rule';
 
+const srcTags = [
+  'audio',
+  'embed',
+  'iframe',
+  'img',
+  'input',
+  'source',
+  'track',
+  'video'
+];
+
 export class SrcNotEmpty extends BaseRule {
   constructor(options: any) {
     super({
@@ -11,12 +22,24 @@ export class SrcNotEmpty extends BaseRule {
   lint = (ast: any, options: Options): any => {
     if (Array.isArray(ast)) {
       ast.forEach((node) => {
-        if (node.attrs) {
-          const { attrs } = node;
-          let { src = [] } = attrs;
-          src = src[0];
-          if(node.name === 'img' && (!src || !src.content)) {
-            this.violation(src || node, options);
+        if(srcTags.includes(node.name)) {
+          if (node.attrs) {
+            const { attrs } = node;
+            let { src = [], type = [] } = attrs;
+            src = src[0];
+            type = type[0]
+            if(!src || !src.content) {
+              /*
+               For only input tags which is of type image,
+               we have to show error if src attribute is empty.
+              */
+              if(node.name === 'input' && type && type.content !== 'image') {
+                return;
+              }
+              this.violation(src || node, options);
+            }
+          } else {
+            this.violation(node, options);
           }
         }
         if (node.content) {
